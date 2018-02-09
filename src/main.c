a2ba026fbf07acaa0de8e2cea95901a0c743ea34
Goksel Goktas <gokselgoktas@gmail.com>
2018-02-09 02:48:39 +0200

    Add command-line UX

--- src/main.c
+++ src/main.c
@@ -42,6 +42,7 @@ struct entry {
 };
 
 static char const *NME_EXECUTABLE_NAME = NULL;
+static char const *NME_INPUT_FILENAME = NULL;
 
 static void report(char const *message, ...)
 {
@@ -113,12 +114,89 @@ static char const *extract_executable_name(char *filename)
     return ++executable_name;
 }
 
+static void display_help_screen(void)
+{
+    printf(
+        "                                             888\n                   "
+        "                          888\n                                      "
+        "       888\n888  888 88888b.  88888b.   8888b.   .d8888b 888  888  .d"
+        "88b.  888d888\n888  888 888 \"88b 888 \"88b     \"88b d88P\"    888 ."
+        "88P d8P  Y8b 888P\"\n888  888 888  888 888  888 .d888888 888      888"
+        "888K  88888888 888\nY88b 888 888  888 888 d88P 888  888 Y88b.    888 "
+        "\"88b Y8b.     888\n \"Y88888 888  888 88888P\"  \"Y888888  \"Y8888P "
+        "888  888  \"Y8888  888\n                  888\n                  888"
+        "\n                  888\n"
+        "\n"
+        "Usage:\n"
+        "        %s [options] file...\n"
+        "\n"
+        "Options:\n"
+        "        -h                  display this help screen\n"
+        "        -v                  display version information\n"
+        "\n",
+        NME_EXECUTABLE_NAME);
+}
+
+static void display_version_information(void)
+{
+    printf("nme-unpacker (%s) version %s [%s]\nauthored in 2018 $ released int"
+        "o the public domain\n", NME_EXECUTABLE_NAME, NME_VERSION_STRING,
+        NME_BUILD_FEATURES);
+}
+
+static void handle_command_line_option(char option, char const *argument)
+{
+    switch (option) {
+    case 'h':
+        display_help_screen();
+        break;
+
+    case 'v':
+        display_version_information();
+        break;
+
+    default:
+        report("unknown option `%c`", option);
+    }
+}
+
+static void parse_command_line(int count, char **arguments)
+{
+    for (int i = 1; i < count; ++i) {
+        char const *argument = arguments[i];
+        char const *parameters = NULL;
+
+        switch (argument[0]) {
+        case '-':
+            if (argument[2] != '\0') {
+                parameters = argument + 2;
+            }
+
+            handle_command_line_option(argument[1], parameters);
+            break;
+
+        default:
+            if (NME_INPUT_FILENAME != NULL) {
+                report("overriding input filename");
+            }
+
+            NME_INPUT_FILENAME = arguments[i];
+        }
+    }
+}
+
 int main(int count, char *arguments[])
 {
     NME_EXECUTABLE_NAME = extract_executable_name(*arguments);
 
     signal(SIGABRT, handle_signal);
     signal(SIGINT, handle_signal);
+
+    parse_command_line(count, arguments);
+
+    if (count == 1 || NME_INPUT_FILENAME == NULL) {
+        fail("no input files");
+    }
 
     return EXIT_SUCCESS;
 }
